6085ab82900253edbc91aca68e0924e6d5d5b439
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-09 00:36:14 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,7 +35,7 @@ def using_uniq(captain_planet_and_the_planeteers)
 end
 
 def using_flatten(private_colleges_in_newyork)
-  
+  private_colleges_in_newyork.flatten
 end
 
 def using_delete(instructors, name)
